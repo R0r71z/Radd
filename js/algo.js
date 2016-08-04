90c62533fea5cdb724be7deb4058a5f60a9015fc
Raddelyn Ortiz <Raddelyn Ortiz>
2016-08-04 15:48:51 -0400

    Made each square independent by adding ID's

--- js/algo.js
+++ js/algo.js
@@ -1,20 +1,34 @@
 
-var black = '<div style="display:block; padding:0px; margin:0px; width:48px; height:48px; float:left; background-color:#17A70D;"></div>';
-var white = '<div style="display:block; padding:0px; margin:0px; width:48px; height:48px; float:left; background-color:#000000;"></div>';
+var green = '<div style="display:block; padding:0px; margin:0px; width:48px; height:48px; float:left; background-color:#17A70D;" id="%data%"></div>';
+var black = '<div style="display:block; padding:0px; margin:0px; width:48px; height:48px; float:left; background-color:#000000;" id="%data%"></div>';
+
+function make_id() {
+	letters = ["A","B","C","D","E","F","G","H"]
+	nums = ["1","2","3","4","5","6","7","8"]
+	var id = []
+	for (e in letters) {
+		for (a in nums) {
+			id.push(letters[e]+nums[a]);
+		}
+	}
+	return id;
+};
+
+var id = make_id();
 
 function make_checkers() {
 	var count = 0;
 	while (count<4) {
 		var i = 0;
 		while ( i < 4 ) {
-			$('#main').append(white);
-			$('#main').append(black);
+			$('#main').append(green.replace("%data%",id.splice(0,1)));
+			$('#main').append(black.replace("%data%", id.splice(0,1)));
 			i += 1
 		}
 		var p = 0;
 		while (p < 4 ) {
-			$('#main').append(black);
-			$('#main').append(white);
+			$('#main').append(black.replace("%data%", id.splice(0,1)));
+			$('#main').append(green.replace("%data%", id.splice(0,1)));
 			p += 1
 		}
 		count += 1
